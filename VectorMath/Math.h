a9ad4f2a291e16dc3002c52741c85eac6c6dba1b
Damian Korczowski <erdroy2@gmail.com>
2020-07-07 22:12:41 +0200

    Fixed RoundUpToPow2 and added original source link

--- VectorMath/Math.h
+++ VectorMath/Math.h
@@ -156,15 +156,31 @@ public:
     {
         return a == 0;
     }
-
-    static unsigned long RoundUpToPow2(unsigned long v)
+    
+    static unsigned int RoundUpToPow2(unsigned int v)
     {
+        // source: http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
         v--;
         v |= v >> 1;
         v |= v >> 2;
         v |= v >> 4;
         v |= v >> 8;
         v |= v >> 16;
+        v++;
+
+        return v;
+    }
+	
+    static unsigned long RoundUpToPow2(unsigned long v)
+    {
+        // source: http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        v--;
+        v |= v >> 1;
+        v |= v >> 2;
+        v |= v >> 4;
+        v |= v >> 8;
+        v |= v >> 16;
+        v |= v >> 32;
         v++;
 
         return v;
